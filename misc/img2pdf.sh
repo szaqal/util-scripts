6e860207123920377d6907fe43ca19a96185f3ef
szaqal <malczyk.pawel@gmail.com>
2021-01-22 14:49:13 +0100

    Scripts

--- misc/img2pdf.sh
+++ misc/img2pdf.sh
@@ -9,15 +9,22 @@
 
 #set -x
 
-FUZZ_FACTOR=${CONV_FUZZ:-10%}
+FUZZ_FACTOR=${CONV_FUZZ:-30%}
 PATTERN=${CONV_PATTERN:-*jpg}
+RESIZE=${CONV_RESIZE:-60%}
+QUALITY=${CONV_QUALITY:-10}
 
 if which convert >/dev/null; then
     find $1 -iname ${PATTERN} \
 	-exec \
 	convert {} \
 	-fuzz ${FUZZ_FACTOR} \
+	-strip \
+	-trim \
 	-monochrome \
+	-quality ${QUALITY} \
+	-resize ${RESIZE} \
+	-crop +390+140 -crop -390-660 \
 	-layers Optimize {}.pdf \;
 else
     echo convert does not exist
